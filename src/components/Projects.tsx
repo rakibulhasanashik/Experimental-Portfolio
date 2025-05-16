18a3062a8a5ce13eb0d4829120cbcea67d5acc8a
rakibulhasanashik <112580058+rakibulhasanashik@users.noreply.github.com>
2025-05-16 17:57:30 +0600

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
 import { ArrowRight, ExternalLink } from "lucide-react";
 import { Link } from "react-router-dom";
 
